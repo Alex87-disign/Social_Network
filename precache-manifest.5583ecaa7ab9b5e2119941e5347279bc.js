914bde82276946593e405852a84898c3428b2b18
Yatsiuk Oleksandr <saj87@bk.ru>
2020-05-28 12:44:24 +0300

    Updates

---
+++ precache-manifest.5583ecaa7ab9b5e2119941e5347279bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbfcce4943e4170307f5fd33eaee94f",
+    "url": "/Social_Network/index.html"
+  },
+  {
+    "revision": "6c7ad18546614145a361",
+    "url": "/Social_Network/static/css/main.1a26deaa.chunk.css"
+  },
+  {
+    "revision": "91d5db0f7cf9e55f9e0b",
+    "url": "/Social_Network/static/js/2.5a892090.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Social_Network/static/js/2.5a892090.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c7ad18546614145a361",
+    "url": "/Social_Network/static/js/main.b803954b.chunk.js"
+  },
+  {
+    "revision": "b71deac337d7a4991add",
+    "url": "/Social_Network/static/js/runtime-main.29f38d05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Social_Network/static/media/logo.5d5d9eef.svg"
+  }
+]);
